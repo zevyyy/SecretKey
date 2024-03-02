311806b2fa1395e59ab13b643216003092a89136
RM207 <rm207@Administrators-iMac-67.local>
2024-03-02 10:51:00 +0800

    commit

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI, Request, HTTPException
 app = FastAPI()
 @app.get("/") 
 async def read_main(token: str = None):
-   if token != "Secret":
+   if token != "Secrety":
       raise HTTPException(status_code=401, detail="Unauthorized")
    return {"message": "Hello, World!"} 
